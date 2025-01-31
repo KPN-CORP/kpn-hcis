b7e0fb3feaf981da693d88f1dbc53aa47619e33f
Kleponaeru <kelvinlie08@gmail.com>
2025-01-31 17:28:27 +0700

    adjust approval dalam kota ticket form

--- resources/views/hcis/reimbursements/businessTrip/approval/dalam-kota/approvalTicketDalamKota.blade.php
+++ resources/views/hcis/reimbursements/businessTrip/approval/dalam-kota/approvalTicketDalamKota.blade.php
@@ -12,7 +12,8 @@
         for ($i = 1; $i <= $ticketCount; $i++) :
             $ticket = $ticketData[$i - 1]; // Use specific data for Dalam Kota
         ?>
-        <div class="card bg-light shadow-none" id="ticket-form-dalam-kota-<?php echo $i; ?>" style="display: <?php echo $i <= $ticketCount ? 'block' : 'none'; ?>;">
+        <div class="card bg-light shadow-none" id="ticket-form-dalam-kota-<?php echo $i; ?>"
+            style="display: <?php echo $i <= $ticketCount ? 'block' : 'none'; ?>;">
             <div class="card-body">
                 <div class="h5 text-uppercase">
                     <b>TICKET <?php echo $i; ?></b>
@@ -21,7 +22,7 @@
                     <div class="col-md-4 mb-2">
                         <label class="form-label">Employee Name</label>
                         <select class="form-select form-select-sm select2" id="noktp_tkt_dalam_kota_<?php echo $i; ?>"
-                            name="noktp_tkt_dalam_kota[]">
+                            name="noktp_tkt_dalam_kota[]" disabled>
                             <option value="" selected>Please Select</option>
                             @foreach ($employees as $employee)
                                 <option value="{{ $employee->ktp }}"
@@ -34,15 +35,15 @@
                     <div class="col-md-4 mb-2">
                         <label class="form-label">From</label>
                         <div class="input-group">
-                            <input class="form-control form-control-sm" name="dari_tkt_dalam_kota[]" type="text"
-                                placeholder="ex. Yogyakarta (YIA)" value="{{ $ticket['dari_tkt'] ?? '' }}">
+                            <input class="form-control form-control-sm bg-light" name="dari_tkt_dalam_kota[]" type="text"
+                                placeholder="ex. Yogyakarta (YIA)" value="{{ $ticket['dari_tkt'] ?? '' }}" readonly>
                         </div>
                     </div>
                     <div class="col-md-4 mb-2">
                         <label class="form-label">To</label>
                         <div class="input-group">
-                            <input class="form-control form-control-sm" name="ke_tkt_dalam_kota[]" type="text"
-                                placeholder="ex. Jakarta (CGK)" value="{{ $ticket['ke_tkt'] ?? '' }}">
+                            <input class="form-control form-control-sm bg-light" name="ke_tkt_dalam_kota[]" type="text"
+                                placeholder="ex. Jakarta (CGK)" value="{{ $ticket['ke_tkt'] ?? '' }}" readonly>
                         </div>
                     </div>
                 </div>
@@ -50,7 +51,7 @@
                     <div class="col-md-6 mb-2">
                         <label class="form-label">Transportation Type</label>
                         <div class="input-group">
-                            <select class="form-select form-select-sm" name="jenis_tkt_dalam_kota[]">
+                            <select class="form-select form-select-sm" name="jenis_tkt_dalam_kota[]" disabled>
                                 <option value="">Select Transportation Type</option>
                                 <option value="Train"
                                     {{ $ticket && $ticket['jenis_tkt'] == 'Train' ? 'selected' : '' }}>Train</option>
@@ -64,7 +65,7 @@
                     </div>
                     <div class="col-md-6 mb-2">
                         <label class="form-label">Ticket Type</label>
-                        <select class="form-select form-select-sm" name="type_tkt_dalam_kota[]">
+                        <select class="form-select form-select-sm" name="type_tkt_dalam_kota[]" disabled>
                             <option value="One Way" {{ $ticket && $ticket['type_tkt'] == 'One Way' ? 'selected' : '' }}>
                                 One Way</option>
                             <option value="Round Trip"
@@ -77,17 +78,19 @@
                     <div class="col-md-6 mb-2">
                         <label class="form-label">Date</label>
                         <div class="input-group">
-                            <input class="form-control form-control-sm" name="tgl_brkt_tkt_dalam_kota[]" type="date"
-                                id="tgl_brkt_tkt_dalam_kota_<?php echo $i; ?>" value="{{ $ticket['tgl_brkt_tkt'] ?? '' }}"
-                                onchange="validateDatesDalamKota(<?php echo $i; ?>)">
+                            <input class="form-control form-control-sm bg-light" name="tgl_brkt_tkt_dalam_kota[]"
+                                type="date" id="tgl_brkt_tkt_dalam_kota_<?php echo $i; ?>"
+                                value="{{ $ticket['tgl_brkt_tkt'] ?? '' }}"
+                                onchange="validateDatesDalamKota(<?php echo $i; ?>)" readonly>
                         </div>
                     </div>
                     <div class="col-md-6 mb-2">
                         <label class="form-label">Time</label>
                         <div class="input-group">
-                            <input class="form-control form-control-sm" name="jam_brkt_tkt_dalam_kota[]" type="time"
-                                id="jam_brkt_tkt_dalam_kota_<?php echo $i; ?>" value="{{ $ticket['jam_brkt_tkt'] ?? '' }}"
-                                onchange="validateDatesDalamKota(<?php echo $i; ?>)">
+                            <input class="form-control form-control-sm bg-light" name="jam_brkt_tkt_dalam_kota[]"
+                                type="time" id="jam_brkt_tkt_dalam_kota_<?php echo $i; ?>"
+                                value="{{ $ticket['jam_brkt_tkt'] ?? '' }}"
+                                onchange="validateDatesDalamKota(<?php echo $i; ?>)" readonly>
                         </div>
                     </div>
                 </div>
@@ -97,17 +100,19 @@
                         <div class="col-md-6">
                             <label class="form-label">Return Date</label>
                             <div class="input-group">
-                                <input class="form-control form-control-sm" name="tgl_plg_tkt_dalam_kota[]" type="date"
-                                    id="tgl_plg_tkt_dalam_kota_<?php echo $i; ?>" onchange="validateDatesDalamKota(<?php echo $i; ?>)"
-                                    value="{{ $ticket['tgl_plg_tkt'] ?? '' }}">
+                                <input class="form-control form-control-sm bg-light" name="tgl_plg_tkt_dalam_kota[]"
+                                    type="date" id="tgl_plg_tkt_dalam_kota_<?php echo $i; ?>"
+                                    onchange="validateDatesDalamKota(<?php echo $i; ?>)"
+                                    value="{{ $ticket['tgl_plg_tkt'] ?? '' }}" readonly>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <label class="form-label">Return Time</label>
                             <div class="input-group">
-                                <input class="form-control form-control-sm" id="jam_plg_tkt_dalam_kota_<?php echo $i; ?>"
-                                    name="jam_plg_tkt_dalam_kota[]" type="time" onchange="validateDatesDalamKota(<?php echo $i; ?>)"
-                                    value="{{ $ticket['jam_plg_tkt'] ?? '' }}">
+                                <input class="form-control form-control-sm bg-light"
+                                    id="jam_plg_tkt_dalam_kota_<?php echo $i; ?>" name="jam_plg_tkt_dalam_kota[]"
+                                    type="time" onchange="validateDatesDalamKota(<?php echo $i; ?>)"
+                                    value="{{ $ticket['jam_plg_tkt'] ?? '' }}" readonly>
                             </div>
                         </div>
                     </div>
@@ -115,20 +120,14 @@
                 <div class="row">
                     <div class="col-md-12 mb-2">
                         <label class="form-label">Information</label>
-                        <textarea class="form-control" name="ket_tkt_dalam_kota[]" rows="3" placeholder="Add ticket details">{{ $ticket['ket_tkt'] ?? '' }}</textarea>
+                        <textarea class="form-control bg-light" name="ket_tkt_dalam_kota[]" rows="3" placeholder="Add ticket details"
+                            readonly>{{ $ticket['ket_tkt'] ?? '' }}</textarea>
                     </div>
-                </div>
-
-                <div class="mt-2">
-                    <button type="button" class="btn btn-sm btn-outline-danger remove-ticket-btn-dalam-kota"
-                        id="remove-ticket-btn-dalam-kota" data-form-id="<?php echo $i; ?>">Remove Data</button>
                 </div>
             </div>
         </div>
         <?php endfor; ?>
     </div>
-    <button type="button" class="btn btn-sm btn-outline-primary add-ticket-btn-dalam-kota" id="add-ticket-btn-dalam-kota">Add Ticket
-        Data</button>
 </div>
 
 <script src="{{ asset('vendor/bootstrap/js/jquery-3.6.0.min.js') }}"></script>
