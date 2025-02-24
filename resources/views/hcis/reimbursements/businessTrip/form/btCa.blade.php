092f6aa7fc7309c2bab1f5929c27aacb7a0992ba
Kleponaeru <kelvinlie08@gmail.com>
2025-02-24 15:54:25 +0700

    add id to list item

--- resources/views/hcis/reimbursements/businessTrip/form/btCa.blade.php
+++ resources/views/hcis/reimbursements/businessTrip/form/btCa.blade.php
@@ -15,66 +15,61 @@
             </div>
             <div class="d-flex flex-column">
                 <ul class="nav mb-2" id="pills-tab" role="tablist">
-                    <li class="nav-item" role="presentation">
+                    <li class="nav-item" role="presentation" id="perdiem-tab-li">
                         <button class="nav-link active" id="pills-perdiem-tab" data-bs-toggle="pill"
                             data-bs-target="#pills-perdiem" type="button" role="tab" aria-controls="pills-perdiem"
                             aria-selected="true">{{ $allowance }}</button>
                     </li>
                     @if (($group_company !== 'KPN Plantations' && $group_company !== 'Plantations') || $job_level_number >= 8)
-                        <li class="nav-item" role="presentation">
+                        <li class="nav-item" role="presentation" id="meals-tab-li">
                             <button class="nav-link" id="pills-meals-tab" data-bs-toggle="pill"
                                 data-bs-target="#pills-meals" type="button" role="tab" aria-controls="pills-meals"
                                 aria-selected="false">Meals</button>
                         </li>
                     @endif
-                    <li class="nav-item" role="presentation">
+                    <li class="nav-item" role="presentation" id="transport-tab-li">
                         <button class="nav-link" id="pills-transport-tab" data-bs-toggle="pill"
                             data-bs-target="#pills-transport" type="button" role="tab"
                             aria-controls="pills-transport" aria-selected="false">Transport</button>
                     </li>
-
-                    <li class="nav-item" role="presentation">
+                    <li class="nav-item" role="presentation" id="accomodation-tab-li">
                         <button class="nav-link" id="pills-accomodation-tab" data-bs-toggle="pill"
                             data-bs-target="#pills-accomodation" type="button" role="tab"
                             aria-controls="pills-accomodation" aria-selected="false">Accomodation</button>
                     </li>
-                    <li class="nav-item" role="presentation">
+                    <li class="nav-item" role="presentation" id="other-tab-li">
                         <button class="nav-link" id="pills-other-tab" data-bs-toggle="pill"
                             data-bs-target="#pills-other" type="button" role="tab" aria-controls="pills-other"
                             aria-selected="false">Others</button>
                     </li>
                 </ul>
+
                 <div class="tab-content" id="pills-tabContent">
                     <div class="tab-pane fade show active" id="pills-perdiem" role="tabpanel"
                         aria-labelledby="pills-perdiem-tab">
-                        {{-- ca perdiem content --}}
                         @include('hcis.reimbursements.businessTrip.form.cashadvancedForm.caPerdiem')
                     </div>
                     @if (($group_company !== 'KPN Plantations' && $group_company !== 'Plantations') || $job_level_number >= 8)
                         <div class="tab-pane fade" id="pills-meals" role="tabpanel" aria-labelledby="pills-meals-tab">
-                            {{-- ca transport content --}}
                             @include('hcis.reimbursements.businessTrip.form.cashadvancedForm.caMeals')
                         </div>
                     @endif
                     <div class="tab-pane fade" id="pills-transport" role="tabpanel"
                         aria-labelledby="pills-transport-tab">
-                        {{-- ca transport content --}}
                         @include('hcis.reimbursements.businessTrip.form.cashadvancedForm.caTransport')
                     </div>
                     <div class="tab-pane fade" id="pills-accomodation" role="tabpanel"
                         aria-labelledby="pills-accomodation-tab">
-                        {{-- ca accommodatioon content --}}
-                        @include('hcis.reimbursements.businessTrip.form.cashadvancedForm.caAccommodation')</div>
+                        @include('hcis.reimbursements.businessTrip.form.cashadvancedForm.caAccommodation')
+                    </div>
                     <div class="tab-pane fade" id="pills-other" role="tabpanel" aria-labelledby="pills-other-tab">
-                        {{-- ca others content --}}
                         @include('hcis.reimbursements.businessTrip.form.cashadvancedForm.caOther')
                     </div>
                 </div>
 
                 <br>
                 <div class="col-md-12 mb-2">
-                    <label class="form-label">Total Cash
-                        Advanced</label>
+                    <label class="form-label">Total Cash Advanced</label>
                     <div class="input-group">
                         <div class="input-group-append">
                             <span class="input-group-text">Rp</span>
@@ -83,7 +78,6 @@
                             min="0" value="0" readonly>
                     </div>
                 </div>
-
                 <div class="col-md-12 mb-2" id="total_bt_ent" style="display:none">
                     <label class="form-label">Total Request</label>
                     <div class="input-group">
