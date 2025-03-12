cc45d54c0e1a88c7cd0855e23200782a327dc348
erziealdrian02 <erzie.aldrian02@gmail.com>
2025-03-12 11:48:51 +0700

    Resolve bug delete Accomodation dan Meals

--- resources/views/hcis/reimbursements/businessTrip/declaration-admin/caDeclarationFormAdmin/caAccommodationDeclare.blade.php
+++ resources/views/hcis/reimbursements/businessTrip/declaration-admin/caDeclarationFormAdmin/caAccommodationDeclare.blade.php
@@ -671,7 +671,7 @@
     </div>
 @else
     <div id="form-container-penginapan">
-        <div id="form-container-bt-transport-1" class="card-body p-2 mb-3" style="background-color: #f8f8f8">
+        <div id="form-container-bt-penginapan-1" class="card-body p-2 mb-3" style="background-color: #f8f8f8">
             <p class="fs-4 text-primary" style="font-weight: bold; ">Accommodation 1</p>
             <div class="card-body bg-light p-2 mb-3">
                 <p class="fs-5 text-primary" style="font-weight: bold;">Accommodation Declaration</p>
