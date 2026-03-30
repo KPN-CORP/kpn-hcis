113e4fa12a424148ebc70960ed8d5288f51262c3
Dali Kewara <01126010010@kpndom.com>
2026-03-30 17:24:25 +0700

    update integrasi ocr

--- app/Http/Controllers/ParkingController.php
+++ app/Http/Controllers/ParkingController.php
@@ -19,6 +19,8 @@ class ParkingController extends Controller
             'employee_id' => $employee_id,
         ]);
 
+        dd($response);
+
         if ($response->successful()) {
             $data = $response->json();
             $token = $data->token ?? '';
