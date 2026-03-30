ee9b7b5f40c26ecd832be46a54b410f679cbbf73
Dali Kewara <01126010010@kpndom.com>
2026-03-30 17:21:19 +0700

    update integrasi ocr

--- app/Http/Controllers/ParkingController.php
+++ app/Http/Controllers/ParkingController.php
@@ -2,14 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Http;
 use App\Http\Controllers\Controller;
 
 class ParkingController extends Controller
 {
-    function __construct() {}
+    function __construct() {
+        $this->auth_user = Auth()->user();
+    }
 
     function index()
     {
-        return redirect()->away('https://apps-stage.hcis.live');
+        $employee_id = $this->auth_user->employee_id;
+
+        $response = Http::acceptJson()->post('http://0.0.0.0:5004/generate-token', [
+            'employee_id' => $employee_id,
+        ]);
+
+        if ($response->successful()) {
+            $data = $response->json();
+            $token = $data->token ?? '';
+            $query = http_build_query([
+                'employee_id' => $employee_id,
+                'token' => $token,
+            ]);
+
+            return redirect()->away('https://apps-stage.hcis.live/auth-token-login?'.$query);
+        }
+
+        return redirect()->away('https://apps-stage.hcis.live/auth-token-login');
     }
 }
