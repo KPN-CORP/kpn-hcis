db41a768f5e3e3752f3c1775093298d7e9bbe319
Dali Kewara <01126010010@kpndom.com>
2026-06-22 14:56:47 +0700

    update

--- resources/views/hcis/reimbursements/cashadv/printDeklarasiCashadv.blade.php
+++ resources/views/hcis/reimbursements/cashadv/printDeklarasiCashadv.blade.php
@@ -1224,7 +1224,7 @@
     <footer>
         <script type="text/php">
             if (isset($pdf)) {
-                $x = 360;
+                $x = 310;
                 $y = 810;
                 $text = "Page {PAGE_NUM} of {PAGE_COUNT} Declaration Cash Advanced No. {{ $transactions->no_declaration ?: $transactions->no_ca }}";
                 $font = null;
