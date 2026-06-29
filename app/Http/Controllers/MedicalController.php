ce7e3224df36b8f9a7eaa505c6ab04c184e76c21
Dali Kewara <dalikewara@gmail.com>
2026-06-29 22:59:19 +0700

    update

--- app/Http/Controllers/MedicalController.php
+++ app/Http/Controllers/MedicalController.php
@@ -86,6 +86,7 @@ class MedicalController extends Controller
             "verif_by",
             "balance_verif",
             "approved_by",
+            "doc_status",
             DB::raw(
                 'SUM(CASE WHEN medical_type = "Maternity" THEN balance ELSE 0 END) as maternity_total',
             ),
