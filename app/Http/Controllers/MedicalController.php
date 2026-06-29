3db295199b79fbd81788cb2523306e5edc2cb8fc
Dali Kewara <dalikewara@gmail.com>
2026-06-29 23:10:28 +0700

    update

--- app/Http/Controllers/MedicalController.php
+++ app/Http/Controllers/MedicalController.php
@@ -2717,6 +2717,7 @@ class MedicalController extends Controller
                     'SUM(CASE WHEN mdc_transactions.medical_type = "Glasses" THEN mdc_transactions.balance ELSE 0 END) as glasses_total',
                 ),
                 "mdc_transactions.status",
+                "mdc_transactions.doc_status",
                 DB::raw(
                     "MAX(mdc_transactions.created_at) as latest_created_at",
                 ),
@@ -2736,6 +2737,7 @@ class MedicalController extends Controller
                 "mdc_transactions.patient_name",
                 "mdc_transactions.disease",
                 "mdc_transactions.status",
+                "mdc_transactions.doc_status",
                 "mdc_transactions.employee_id",
             )
             ->orderBy("latest_created_at", "desc")
