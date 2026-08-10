8412bedb7db21163c32d29fe7d9cca4b6115b4ec
Dali Kewara <01126010010@kpndom.com>
2026-08-10 13:08:54 +0700

    fix medical plafond maternity glasses

--- app/Http/Controllers/MedicalController.php
+++ app/Http/Controllers/MedicalController.php
@@ -298,7 +298,8 @@ class MedicalController extends Controller
 
                     if (!$existingHealthPlan) {
                         if ($plafond_lists->medical_type == "Maternity") {
-                            $balance = $plafond_lists->balance * ($bulan / 12);
+                            // $balance = $plafond_lists->balance * ($bulan / 12);
+                            $balance = $plafond_lists->balance;
                         } elseif ($plafond_lists->medical_type == "Inpatient") {
                             $balance = $plafond_lists->balance * ($bulan / 12);
                         } elseif (
@@ -306,7 +307,8 @@ class MedicalController extends Controller
                         ) {
                             $balance = $plafond_lists->balance * ($bulan / 12);
                         } elseif ($plafond_lists->medical_type == "Glasses") {
-                            $balance = $plafond_lists->balance * ($bulan / 12);
+                            // $balance = $plafond_lists->balance * ($bulan / 12);
+                            $balance = $plafond_lists->balance;
                         }
 
                         $newHealthPlan = HealthPlan::create([
